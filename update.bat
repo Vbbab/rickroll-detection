69292d031900a8ae8bae3a5af972259d98c421f9
Vbbab <44555590+Vbbab@users.noreply.github.com>
2021-01-01 19:13:14 -0800

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,4 +1,11 @@
 @echo off
 echo Checking prerequisites.
 
-REM try to determine if the user downloaded as .zip from gith
+REM try to determine if the user downloaded as .zip from github
+if exist .git (
+  echo Updating...
+  git pull
+) else (
+  REM if they didn't pull a git repo, probably they don't have git
+  echo This is a stationary download, can't pull updates from Github. Please download the repo again to update it.
+)
